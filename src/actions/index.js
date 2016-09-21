9cdce77c96be8bb0c4a19f90537f9571d91f4679
onzag <edward@aa.wtf>
2016-09-21 12:42:25 +0200

    added actions list

---
+++ src/actions/index.js
@@ -0,0 +1,150 @@
+/**
+ * @file Contains the action generators
+ * @author Edward Gonzalez
+ */
+
+import * as ActionTypes from '../constants/ActionTypes';
+import * as AbortReasons from '../constants/AbortReasons';
+import * as WithdrawFailReasons from '../constants/WithdrawFailReasons';
+
+/*
+ * @ignore
+ */
+const AbortReasonsList = Object.keys(AbortReasons);
+
+/*
+ * @ignore
+ */
+const WithdrawFailReasonsList = Object.keys(WithdrawFailReasons);
+
+/**
+ * Executes when the card has been inserted by the user
+ * @return {Object} The generated action
+ * @property {String} type The type constant as defined in ActionTypes
+ */
+export const cardInserted = ()=>{
+	return {'type':ActionTypes.CARD_INSERTED}
+}
+
+/**
+ * Executes when the card has been processed by the ATM
+ * @return {Object} The generated action
+ * @property {String} type The type constant as defined in ActionTypes
+ */
+export const cardProcessed = ()=>{
+	return {'type':ActionTypes.CARD_PROCESSED}
+}
+
+/**
+ * Executes when the pin has been inserted by the user
+ * @return {Object} The generated action
+ * @property {String} type The type constant as defined in ActionTypes
+ * @property {String} payload The inserted pin
+ */
+export const pinInserted = (pin)=>{
+	if (!pin || typeof(pin) !== "string"){
+		throw new Error('cannot execute action pinInserted without a valid pin',pin);
+	}
+	return {'type':ActionTypes.PIN_INSERTED,'payload':pin};
+}
+
+/**
+ * Executes when the pin processed is valid
+ * @return {Object} The generated action
+ * @property {String} type The type constant as defined in ActionTypes
+ */
+export const pinProcessedValid = ()=>{
+	return {'type':ActionTypes.PIN_PROCESSED_VALID};
+}
+
+/**
+ * Executes when the pin processed is not valid
+ * @return {Object} The generated action
+ * @property {String} type The type constant as defined in ActionTypes
+ */
+export const pinProcessedInvalid = ()=>{
+	return {'type':ActionTypes.PIN_PROCESSED_INVALID};
+}
+
+/**
+ * Executes when the user specifies that he wants to withdraw some money
+ * @return {Object} The generated action
+ * @property {String} type The type constant as defined in ActionTypes
+ */
+export const withdraw = (amount)=>{
+	if (!amount || typeof(amount) !== "number" || amount < 0){
+		throw new Error('cannot execute action withdraw without a valid amount',amount);
+	}
+	return {'type':ActionTypes.WITHDRAW,'payload':amount};
+}
+
+/**
+ * Executes when the withdraw was suscesful
+ * @return {Object} The generated action
+ * @property {String} type The type constant as defined in ActionTypes
+ */
+export const withdrawSuscesful = ()=>{
+	return {'type':ActionTypes.WITHDRAW_SUSCESFUL};
+}
+
+/**
+ * Executes when the withdraw has failed
+ * @return {Object} The generated action
+ * @property {String} type The type constant as defined in ActionTypes
+ * @property {String} payload The inserted pin as defined in WithdrawFailReasons
+ */
+export const withdrawFailed = (reason)=>{
+	if (WithdrawFailReasonsList.indexOf(reason) === -1){
+		throw new Error('cannot execute action withdrawFailed without a valid reason',reason,'from',WithdrawFailReasonsList);
+	}
+	return {'type':ActionTypes.WITHDRAW_SUSCESFUL,'payload':reason};
+}
+
+/**
+ * Executes when the money is delivered
+ * @return {Object} The generated action
+ * @property {String} type The type constant as defined in ActionTypes
+ */
+export const moneyDelivered = ()=>{
+	return {'type':ActionTypes.MONEY_DELIVERED};
+}
+
+/**
+ * Executes when the user takes the card and the money
+ * @return {Object} The generated action
+ * @property {String} type The type constant as defined in ActionTypes
+ */
+export const cardAndMoneyTaken = ()=>{
+	return {'type':ActionTypes.CARD_AND_MONEY_TAKEN};
+}
+
+/**
+ * Executes when the ATM is done
+ * @return {Object} The generated action
+ * @property {String} type The type constant as defined in ActionTypes
+ */
+export const finished = ()=>{
+	return {'type':ActionTypes.FINISHED};
+}
+
+/**
+ * Executes when the ATM aborts for some reason
+ * @return {Object} The generated action
+ * @property {String} type The type constant as defined in ActionTypes
+ * @property {String} payload The reason why it aborts as defined in AbortReasons
+ */
+export const performAbort = (reason)=>{
+	if (AbortReasonsList.indexOf(reason) === -1){
+		throw new Error('cannot execute action performAbort without a valid reason',reason,'from',AbortReasonsList);
+	}
+	return {'type':ActionTypes.PERFORM_ABORT,'payload':reason};
+}
+
+/**
+ * Executes when the ATM completes the abort process
+ * @return {Object} The generated action
+ * @property {String} type The type constant as defined in ActionTypes
+ */
+export const completeAbort = ()=>{
+	return {'type':ActionTypes.COMPLETE_ABORT};
+}
