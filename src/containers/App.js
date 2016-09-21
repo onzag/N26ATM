85dccc4b9743b11d5315d82050b0226e747cb0af
onzag <edward@aa.wtf>
2016-09-21 12:41:58 +0200

    Added connection with the store to the main app handler

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,12 +4,23 @@
  */
 
 import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import * as actions from '../actions';
+import * as controllers from '../controllers';
+import * as States from '../constants/States';
 
 /** 
  * This is the main application class that controls all the components that render a specific view
  * It uses the state out of the store in order to render the view
  */
-export default class App extends React.Component {
+class App extends React.Component {
+	static propTypes = {
+		ATMState: React.PropTypes.shape({
+			'state':React.PropTypes.oneOf(Object.keys(States)).isRequired
+		})
+	}
 	/**
 	 * Render function
 	 * @returns {React.Component}
@@ -20,3 +31,24 @@ export default class App extends React.Component {
 		</div>);
 	}
 }
+
+/**
+ * @ignore
+ */
+const mapStateToProps = (state)=>{
+	return {
+		ATMState: state
+	}
+};
+
+/**
+ * @ignore
+ */
+const mapDispatchToProps = (dispatch)=>{
+	return {actions: bindActionCreators(actions, dispatch)}
+};
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(App);
