9a8585e691e8a367185342cb48d698c4429c61da
onzag <edward@aa.wtf>
2016-09-21 20:08:34 +0200

    added configuration variables

---
+++ src/constants/config.js
@@ -0,0 +1,14 @@
+/**
+ * @file Contains the configuration of the ATM
+ * @author Edward Gonzalez
+ */
+
+/**
+ * The maximum times you can retry to input the pin
+ */
+export const MAX_PIN_TRIES = 3;
+
+/**
+ * Right pin number
+ */
+export const RIGHT_PIN = "1234";
