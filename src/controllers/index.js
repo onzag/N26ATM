2d4109ee530f415bdf27c0f3df4c8ff4f9e1410c
onzag <edward@aa.wtf>
2016-09-21 21:13:04 +0200

    added waiting for amount controller into export list

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,8 @@
 import WaitingForCard from './WaitingForCard';
 import ProcessingCard from './ProcessingCard';
 import WaitingForPin from './WaitingForPin';
+import WaitingForAmount from './WaitingForAmount';
 import Aborting from './Aborting';
 import ProcessingPin from './ProcessingPin';
 
-export {WaitingForCard,ProcessingCard,WaitingForPin,Aborting,ProcessingPin};
+export {WaitingForCard,ProcessingCard,WaitingForPin,Aborting,ProcessingPin,WaitingForAmount};
